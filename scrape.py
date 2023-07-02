3aa3578b595dd5781b9d8ef266e161972c12f48e
illumi-410 <sandeepparajuli96@gmail.com>
2023-07-02 23:35:44 +0545

    ajax=True

--- scrape.py
+++ scrape.py
@@ -1,74 +1,43 @@
-from selenium import webdriver
-import os
+import requests
 import csv
-import time
-from bs4 import BeautifulSoup
 
-os.environ['PATH'] += r"D:\Drivers\chromedriver.exe"
+def get_food_items():
+    search_term = 'food'
+    page = 1
+    last_page = 20
 
-driver = webdriver.Chrome()
+    while True:
+        url = f'https://www.daraz.com.np/catalog/?page={page}&q={search_term}&ajax=True'
+        response = requests.get(url)
+        
+        if response.status_code != 200:
+            print(f'Error while getting data for page {page}')
+            return
+        
+        data = response.json()
 
-driver.get('https://www.daraz.com.np/catalog/?q=food')
+        filter_items = data['mods']['filter']['filterItems']
+        
+        with open('output.csv', 'a', newline='') as file:
+            writer = csv.writer(file)
 
-time.sleep(3)
+            if page == 1:
+                writer.writerow(['value', 'title','url','order', 'id'])
 
-html = driver.page_source
+            for item in filter_items:
+                options = item['options'] if 'options' in item else []
+                for option in options:
+                    value = option['value']
+                    title = option['title']
+                    order = option['order']
+                    url = option['url'] if 'url' in option else ''
+                    id_ = option['id']  if 'id' in option else ''
+                    writer.writerow([value, title,url, order, id_])
+        
+        
+        if page == last_page:
+            break
+        
+        page += 1
 
-soup = BeautifulSoup(html, 'html.parser')
-
-items = soup.find_all('div', {'class':'box--pRqdD'})
-
-food_data = {}
-for item in items:
-    name = item.find('div', {'class':'title--wFj93'}).text.strip()
-    price = item.find('span', {'class':'currency--GVKjl'}).text.strip()
-    discount_element = item.find('span', {'class':'discount--HADrg'})
-
-    
-    if discount_element is not None:
-        discount = discount_element.text.strip()
-    else:
-        discount = 'None'
-
-    food_data[name] = {'price': price, 'discount': discount}
-
-
-with open('output.csv', 'w') as file:
-    writer = csv.DictWriter(file, fieldnames=['Name', 'Price', 'Discount'])
-    writer.writeheader()
-    for name, data in food_data.items():
-        writer.writerow({'Name': name , 'Price': data['price'], 'Discount': data['discount']})
-
-
-driver.quit()
-
-
-
-
-
-
-
-
-
-
-# import requests
-# from bs4 import BeautifulSoup
-
-# def get_foot_items():
-#     search_term = 'food'
-#     url = f'https://www.daraz.com.np/catalog/?q={search_term}'
-
-#     response = requests.get(url)
-    
-#     if response.status_code != 200:
-#         print('Error while getting data')
-#         return
-    
-#     soup = BeautifulSoup(response.content, 'html.parser')
-#     print(soup)
-#     food_items = soup.find_all('div', {'class':'box--pRqdD'})
-
-#     print(food_items)
-
-
-# get_foot_items()
+get_food_items()
