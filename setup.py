aa1b8baee8945d26d3ac82b969dc77f21a6ec6ee
illumi-410 <sandeepparajuli96@gmail.com>
2023-07-07 22:12:46 +0545

    minor changes

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name = 'scrape',
+    version = '1.0.0',
+    description = 'Project to scrape price and title from daraz and parse the unit from the title',
+    author='Sandeep Parajuli',
+    packages=['scrape'],
+)
